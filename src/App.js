372de6252549b5f863ca29ff6c3754d98d182f7a
Gustavo <gcrodrigues413@gmail.com>
2020-05-28 10:45:43 -0300

    Getting started with redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, signIn, signOut } from "./actions";
 
-function App() {
+import "./style.css";
+
+const App = () => {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      {isLogged ? (
+        <div className="wrapper">
+          <h1 className="counter">Counter: {counter}</h1>
+          <button className="button" onClick={() => dispatch(increment())}>
+            Increment
+          </button>
+          <button className="button" onClick={() => dispatch(decrement())}>
+            Decrement
+          </button>
+          <button className="signOut" onClick={() => dispatch(signOut())}>
+            Sign Out
+          </button>
+        </div>
+      ) : (
+        <button
+          className="signButton signIn"
+          onClick={() => dispatch(signIn())}
         >
-          Learn React
-        </a>
-      </header>
+          Sign in
+        </button>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import counterReducer from "./counter";
+import loggedReducer from "./isLogged";
+
+import { combineReducers } from "redux";
+
+const reducers = combineReducers({
+  counter: counterReducer,
+  isLogged: loggedReducer,
+});
+
+export default reducers;
